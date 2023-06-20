40bead72c88d47eddda7066d29167cc3bea58ddf
yanghao05 <yanghao05@qianxin.com>
2023-06-20 19:15:10 +0800

    support gen enum

---
+++ cmd/gen_enum.go
@@ -0,0 +1,100 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"git-open.qianxin-inc.cn/free/csmp/atomctl/utils"
+	"git-open.qianxin-inc.cn/free/csmp/atomctl/utils/generator"
+	"github.com/spf13/cobra"
+)
+
+var genEnumCmd = &cobra.Command{
+	Use:     "enum",
+	Short:   "generate enum for consts",
+	Example: "atomctl gen enum",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		var filenames []string
+
+		pwd, err := os.Getwd()
+		if err != nil {
+			return err
+		}
+
+		err = filepath.Walk(pwd, func(path string, info fs.FileInfo, err error) error {
+			if utils.IsDir(path) {
+				return nil
+			}
+
+			if !strings.HasSuffix(path, ".go") {
+				return nil
+			}
+
+			content, err := os.ReadFile(path)
+			if err != nil {
+				return err
+			}
+
+			if strings.Contains(string(content), "ENUM(") && strings.Contains(string(content), "swagger:enum") {
+				filenames = append(filenames, path)
+			}
+			return nil
+		})
+
+		if err != nil {
+			return err
+		}
+
+		if len(filenames) == 0 {
+			return fmt.Errorf("no enum files found in %s", pwd)
+		}
+
+		g := generator.NewGenerator()
+
+		g.WithMarshal()
+		g.WithSQLDriver()
+		g.WithSQLInt()
+		g.WithSQLNullInt()
+		g.WithSQLNullStr()
+
+		for _, fileName := range filenames {
+			log.Printf("Generating enums for %s", fileName)
+
+			fileName, _ = filepath.Abs(fileName)
+			outFilePath := fmt.Sprintf("%s_enum.go", strings.TrimSuffix(fileName, filepath.Ext(fileName)))
+			if strings.HasSuffix(fileName, "_test.go") {
+				outFilePath = strings.Replace(outFilePath, "_test_enum.go", "_enum_test.go", 1)
+			}
+
+			// Parse the file given in arguments
+			raw, err := g.GenerateFromFile(fileName)
+			if err != nil {
+				return fmt.Errorf("failed generating enums\nInputFile=%s\nError=%s", fileName, err)
+			}
+
+			// Nothing was generated, ignore the output and don't create a file.
+			if len(raw) < 1 {
+				continue
+			}
+
+			mode := int(0o644)
+			err = os.WriteFile(outFilePath, raw, os.FileMode(mode))
+			if err != nil {
+				return fmt.Errorf("failed writing to file %s: %s", outFilePath, err)
+			}
+		}
+
+		return nil
+	},
+}
+
+func init() {
+	genCmd.AddCommand(genEnumCmd)
+}
--- go.mod
+++ go.mod
@@ -3,16 +3,36 @@ module github.com/rogeecn/atomctl
 go 1.19
 
 require (
+	git-open.qianxin-inc.cn/free/csmp/atomctl v0.0.0-20230620111318-414a70688fd6
+	github.com/Masterminds/sprig/v3 v3.2.3
+	github.com/bradleyjkemp/cupaloy/v2 v2.8.0
 	github.com/gertd/go-pluralize v0.2.1
 	github.com/iancoleman/strcase v0.2.0
 	github.com/pkg/errors v0.8.1
 	github.com/samber/lo v1.38.1
 	github.com/spf13/cobra v1.6.1
+	github.com/stretchr/testify v1.8.4
 	golang.org/x/mod v0.10.0
+	golang.org/x/text v0.4.0
+	golang.org/x/tools v0.1.12
 )
 
 require (
+	github.com/Masterminds/goutils v1.1.1 // indirect
+	github.com/Masterminds/semver/v3 v3.2.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/uuid v1.1.1 // indirect
+	github.com/huandu/xstrings v1.3.3 // indirect
+	github.com/imdario/mergo v0.3.11 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/mitchellh/copystructure v1.0.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/shopspring/decimal v1.2.0 // indirect
+	github.com/spf13/cast v1.3.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/crypto v0.3.0 // indirect
 	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
+	golang.org/x/sys v0.2.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
